4b621306b4dfe0bf37130bd9efad84a400b48e1c
Lovas Bertalan <lovas.bertalan97@gmail.com>
2023-03-05 17:00:05 +0100

    P5 line drawing working

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
         <Header/>
-        <Canvas/>
+        <Canvas canvasHeight={window.innerHeight} canvasWidth={window.innerWidth}/>
     </div>
   )
 }
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,116 +1,66 @@
-import React, { useEffect } from 'react'
-import rough from 'roughjs'
-import { Drawable } from 'roughjs/bin/core'
-import Element from '../interfaces/Element'
+import React, { useRef, useEffect } from 'react';
+import p5 from 'p5';
 
+type Props = {
+  canvasWidth: number;
+  canvasHeight: number;
+};
 
-const Canvas = () => {
-    const generator = rough.generator()
-    const [elements, setElements] = React.useState([] as Element[])
-    const [drawing, setDrawing] = React.useState(false as boolean)
+const Canvas: React.FC<Props> = ({ canvasWidth, canvasHeight }) => {
+  const canvasParentRef = useRef<HTMLDivElement>(null);
+  const p5Ref = useRef<p5>();
+  const startXRef = useRef(0);
+  const startYRef = useRef(0);
+  const isDrawing = useRef(false);
 
-    useEffect(() => {
-        const canvas = document.getElementById('canvas') as HTMLCanvasElement
-        const context = canvas.getContext('2d') as CanvasRenderingContext2D
-        context.clearRect(0, 0, canvas.width, canvas.height)
+  const setupSketch = (p: p5, canvasParentElement: Element | null) => {
+    if (canvasParentElement) {
+      // Set up canvas
+      const canvas = p.createCanvas(canvasWidth, canvasHeight);
+      canvas.parent(canvasParentElement);
+  
+      // Set up stroke
+      p.stroke(0);
+      p.strokeWeight(10);
+      p.noFill();
+  
+      // Draw straight line on mouse release
+      canvas.mousePressed(() => {
+        startXRef.current = p.mouseX;
+        startYRef.current = p.mouseY;
+        isDrawing.current = true;
+      });
+  
+      canvas.mouseReleased(() => {
+        isDrawing.current = false;
+        p.rect(startXRef.current, startYRef.current, p.mouseX, p.mouseY);
+      });
+    }
+  };
+  
 
-        const roughCanvas = rough.canvas(canvas)
-
-        elements.forEach(({roughElement}) => {
-            roughCanvas.draw(roughElement as Drawable)
-        })
-        
-
-    }, [elements])
-
-    //V2
-    const createElement = (x1: number, y1: number, x2: number, y2: number) =>{
-        const roughElement = generator.line(x1, y1, x2, y2)
-        return { x1, y1, x2, y2, roughElement}
+  useEffect(() => {
+    if (canvasParentRef.current) {
+      // Create new p5 instance with sketch
+      p5Ref.current = new p5((p: p5) => {
+        setupSketch(p, canvasParentRef.current);
+      });
     }
 
-    const handleMouseDown = (e: React.MouseEvent) => {
-        setDrawing(true)
-        const {clientX, clientY } = e
-        
-        const element = createElement(clientX, clientY, clientX, clientY)
-        setElements(prevState => [...prevState, element])
+    // Clean up p5 instance on unmount
+    return () => {
+      p5Ref.current?.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    // Resize canvas when props change
+    if (p5Ref.current) {
+      p5Ref.current.resizeCanvas(canvasWidth, canvasHeight);
     }
+  }, [canvasWidth, canvasHeight]);
 
-    const handleMouseMove = (event: React.MouseEvent) => {
-        if(!drawing) return
+  return <div ref={canvasParentRef} />;
+};
 
-        const {clientX, clientY } = event
-
-        const lastIndex : number = elements.length - 1
-        const { x1, y1 } = elements[lastIndex]
-
-        const updatedElement = createElement(x1, y1, clientX, clientY)
-
-        const tempElements = [...elements]
-        tempElements[lastIndex] = updatedElement
-        setElements(tempElements)
-        console.log({lastIndex})
-    }
-
-    const handleMouseUp = (e: React.MouseEvent) => {
-        setDrawing(false)
-    }
-    
-    /*
-    //V1
-
-    const createElement = (x1: number, y1: number, x2: number, y2: number) => {
-        const roughElement = generator.line(x1, y1, x2, y2);
-        return {x1, y1, x2, y2, roughElement}
-    }
-
-    const handleMouseDown = (e: React.MouseEvent) => {
-        setDrawing(true)
-        const { clientX, clientY } = e as React.MouseEvent
-        const element = createElement(clientX, clientY, clientX, clientY)
-        setElements((prevState)=>[...prevState, element])
-    }
-
-    const handleMouseMove = (e: React.MouseEvent) => {
-        if (drawing) {
-            const { clientX, clientY } = e as React.MouseEvent
-            const lastIndex : number = elements.length - 1
-            const { x1, y1 } = elements[lastIndex]
-            const updatedElement = createElement(x1, y1, clientX, clientY)
-            
-            const elementsCopy = [...elements]
-            elementsCopy[lastIndex] = updatedElement
-            setElements(elementsCopy)
-            console.log(x1, y1, clientX, clientY)
-        }
-    }
-    
-
-    const handleMouseUp = (e: React.MouseEvent) => {
-        setDrawing(false)
-    }
-*/
-
-    const canvasStyle = {
-        /*width: window.innerWidth,
-        height: window.innerHeight,
-        */
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#424242',
-      }
-
-    return (
-        <canvas 
-            id='canvas' 
-            style={canvasStyle}
-            onMouseDown={handleMouseDown}
-            onMouseMove={handleMouseMove}
-            onMouseUp={handleMouseUp}
-        >
-        </canvas>
-    )
-}
-
-export default Canvas
+export default Canvas;
